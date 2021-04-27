fdfba53298c2017d8893ea4fe966aa235d604a0d
Koosha Tahmasebipour <kooshi.ml@gmail.com>
2021-04-27 09:57:27 -0400

    Transformer package

--- Transformer/setup.py
+++ Transformer/setup.py
@@ -1,9 +1,30 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
-    name = "deeplegos",
-    packages = find_packages(where='src'),
-    package_dir = {'':'src'},
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as requirements_file:
+    external_packages = requirements_file.read()
+
+setuptools.setup(
+    name="Transformer",
+    version="0.0.1",
+    author="Koosha Tahm",
+    author_email="kooshi.ml@gmail.com",
+    description="Transformer, Attention is all you need! Used http://nlp.seas.harvard.edu/2018/04/03/attention.html as the main reference.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/koosha-t/Attention_is_ALL_you_need.git",
+    project_urls={
+        "Bug Tracker": "https://github.com/koosha-t/Attention_is_ALL_you_need/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = external_packages,
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
 )
-
-
