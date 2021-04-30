09d93e5ed69545a25c2f1860ad1dce601128284c
Koosha Tahmasebipour <kooshi.ml@gmail.com>
2021-04-30 15:52:36 -0400

    layernorm implementation

--- Transformer/src/LayerNorm.py
+++ Transformer/src/LayerNorm.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 """
@@ -7,5 +8,17 @@ where Sublayer(x) is the function implemented by the sub-layer itself.
 """
 
 class LayerNorm(nn.Module):
-    def __init__(self):
-        pass
+    " Construct a layernorm module "
+    def __init__(self, features, eps=1e-6):
+        super(LayerNorm,self).__init__()
+        self.a_2 = nn.Parameter(torch.ones(features))
+        self.b_2 = nn.Parameter(torch.zeros(features))
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim = True)
+        std = x.std(-1, keepdim=True)
+        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
+
+
+
